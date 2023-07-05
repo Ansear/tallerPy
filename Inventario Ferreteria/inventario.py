6bd33c97340eb2aba9d8e06511e00ded20226815
Raz <santiago.gonzalezagudelo@gmail.com>
2023-07-05 07:52:20 -0500

    compras

--- Inventario Ferreteria/inventario.py
+++ Inventario Ferreteria/inventario.py
@@ -1,40 +1,32 @@
 import os
 import producto
 if __name__ == "__main__":
-    os.system("cls")
+    os.system("clear")
     proveedores = {}
+    compras  = []
     pro = True
     while pro: 
-        os.system("cls")
+        os.system("clear")
         print("1.CLIENTE\n2.ADMINISTRADOR\nSeleccione una opcion: ")
         op = int(input(":)"))
         if(op == 1):
-            os.system("cls")
-            print('+','-'*76,'+')
-            print("|{:^30}{:^20}{:^28}|".format(" ","Lista de productos "," "))
-            print('+','-'*76,'+')
-            print("|{:^15}|{:^15}|{:^15}|{:^15}|{:^14}|".format('Codigo','Nombre','Estado','Precio','Proveedor'))
-            if(len(proveedores) >= 1):
-                for key,value in proveedores.items():
-                    for j,k in value.items():
-                        print('+','-'*76,'+')
-                        print("|{:^15}|{:^15}|{:^15}|{:^15}|{:^15}|".format(k["codigo"],k["nombre"],k["estado"],k["precio"],key))
-                        print('+','-'*76,'+')
-                print("Presione enter para continuar...")
-                input()
-            else:
-                print("No hay productos registrados\n")
-                print("Presione enter para continuar...")
-                input()
-        if(op == 2):
-            os.system("cls")
+            os.system("clear")
+            print("1.Ver productos") 
+            producto.showProducts(proveedores)
+            print("Presione enter para continuar...")
+            input()
+        elif(op == 2):
+            os.system("clear")
             print("1.Agregar proveedores: ")
             print("2.Agregar productos: ")
+            print("3.Ver Compras: ")
             op= int(input("Selecciona una opcion: "))
             if(op == 1):
                 producto.addProveedor(proveedores)
             elif(op == 2):
-                producto.addProduct(proveedores)
+                producto.addProduct(proveedores,compras)
+            elif(op == 3):
+                print(compras)
         
         pro = input("¿Desea Continuar En el programa? S(Si) Enter(No)")
 
--- Inventario Ferreteria/producto.py
+++ Inventario Ferreteria/producto.py
@@ -3,10 +3,10 @@ import os
 def addProveedor(proveedores: dict):
     addProveedor =  True
     while addProveedor:
-        os.system("cls")
+        os.system("clear")
         op = bool(input("¿Desea Agregar un proveedor? S(si) Enter(No): "))
         if(op):
-            os.system("cls")
+            os.system("clear")
             provee = input("Ingrese el nombre del proveedor: ")
             provee = provee.upper()
             proveedor = {
@@ -18,22 +18,22 @@ def addProveedor(proveedores: dict):
             print("Presiona enter para continuar....")
             input()
 
-def addProduct(proveedores: dict):
+def addProduct(proveedores: dict,compras :list):
     addProduct = True
-
+    numCompra = 0
     while addProduct:
-        os.system("cls")
+        os.system("clear")
         name = input("Ingresa el nombre del proveedor: ")
         name = name.upper()
         if name in proveedores:
             op = bool(input("¿Desea agregar un producto? S(Si) Enter(No): "))
-            if(op):
-                os.system("cls")
+            if(op == 1):
+                os.system("clear")
                 cod = input("Ingrese el cod del producto: ")
                 nombre = input("Ingrese el nombre del producto: ")
-                precio = input("Ingrese el precio del producto: ")
+                precio = int(input(f"Ingrese el precio del producto: {nombre} c/u: "))
                 stockMinimo = input("Ingrese el stock minimo del producto: ")
-                stockMaximo = input("Ingrese el stock maximo del producto: ")
+                stockMaximo = int(input("Ingrese el stock maximo del producto: "))
                 estado = bool(input("Ingrese el estado del producto: D(Disponible) Enter(No Disponible)"))
                 if(estado == True):
                     estado = "Disponible"
@@ -53,6 +53,32 @@ def addProduct(proveedores: dict):
                 }
                 
                 proveedores[name].update(producto)
+                nomPro = name
+                numCompra += 1
+                os.system("clear")
+                fecha = input("Ingrese la fecha (DD/MM/AAAA): ")
+                valCompra = stockMaximo * precio
+                
+                numCompra = {
+                    "proveedor":name,
+                    "producto":nombre,
+                    "Numero Compra":numCompra,
+                    "Fecha Compra":fecha,
+                    "Valor Compra":valCompra,
+                    "Cantidad Comprada":stockMaximo
+                }
+                compras.append(numCompra)            
+
+                op = bool(input("¿Desea agregar otro producto? 1.(Si) 2(No)"))
+                if(op == 1):
+                    addProducto = True
+                elif(op == 2):
+                    addProducto = False
+                else:
+                    print("Ingrese un dato valido")
+                    print("Presiona enter para continuar....")
+                    input()
+                    addProducto = False
             else:
                 addProduct = False
         else: 
@@ -61,6 +87,28 @@ def addProduct(proveedores: dict):
             input()
             addProduct = False
 
+def showProducts(proveedores):
+    print('+','-'*76,'+')
+    print("|{:^30}{:^20}{:^28}|".format(" ","Lista de productos "," "))
+    print('+','-'*76,'+')
+    print("|{:^15}|{:^15}|{:^15}|{:^15}|{:^14}|".format('Codigo','Nombre','Estado','Precio','Proveedor'))
+    if(len(proveedores) >= 1):
+        for key,value in proveedores.items():
+            for j,k in value.items():
+                print('+','-'*76,'+')
+                print("|{:^15}|{:^15}|{:^15}|{:^15}|{:^15}|".format(k["codigo"],k["nombre"],k["estado"],k["precio"],key))
+                print('+','-'*76,'+')
+    else:
+            print("No hay productos registrados\n")
+            print("Presione enter para continuar...")
+            input()
+
+def compra(proveedores):
+    print("Ingrese la cantidad de produtos a comprar: ")                
+    pro = int(input("Ingrese el producto que necesita comprar: "))
+    cant = int(input("ingrese la cantidad de produtos a comprar: "))
+    fecha = input("Ingrese la fecha (DD/MM/AAAA):")
+    
 # {
 #     "juan":{
 #         "cod":{},
